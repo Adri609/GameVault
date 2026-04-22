78d26825df39f1bc55a749cb81fb3250dabdf561
Bogdan Adrián Tucaciuc <adri.bog3@gmail.com>
2026-04-22 14:10:38 +0200

    feat: setup retrofit, okhttp, and hilt network module with secure buildconfig keys

---
+++ app/src/main/java/com/gamevault/data/remote/IgdbApi.kt
@@ -0,0 +1,7 @@
+package com.gamevault.data.remote
+
+import retrofit2.http.GET
+
+interface IgdbApi {
+    // TODO: Aquí se agregarán las llamadas a las APIs
+}
---
+++ app/src/main/java/com/gamevault/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.gamevault.di
+
+import com.gamevault.BuildConfig
+import com.gamevault.data.remote.IgdbApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        // Esto sirve para ver las peticiones de red en la consola
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        // Inyectar las claves secretas en todas las peticiones automáticamente
+        val authInterceptor = Interceptor { chain ->
+            val request = chain.request().newBuilder()
+                .addHeader("Client-ID", BuildConfig.IGDB_CLIENT_ID)
+                .addHeader("Authorization", "Bearer ${BuildConfig.IGDB_ACCESS_TOKEN}")
+                .build()
+            chain.proceed(request)
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://api.igdb.com/v4/") // URL oficial de IGDB
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideIgdbApi(retrofit: Retrofit): IgdbApi {
+        return retrofit.create(IgdbApi::class.java)
+    }
+}
